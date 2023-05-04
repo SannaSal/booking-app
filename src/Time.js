191c716ccb3fd75eebb214df29bf85fab6fff170
Sanna Salminen <sanna.salminen@edu.turkuamk.fi>
2023-05-04 08:57:56 +0300

    Testing2

--- src/Time.js
+++ src/Time.js
@@ -7,7 +7,7 @@ function Time(props) {
   return (
     <div>
       {props.showTime ?<h4>{props.date.toLocaleString("fi-FI", {day:'numeric', month:'numeric', year:'numeric'})}</h4> : null}
-      {props.showTime ? <h3>2. Valitse aika::</h3> : null}
+      {props.showTime ? <h3>2. Valitse aika:</h3> : null}
       {props.showTime ? <Times date={props.date}/> : null}
     </div>
   )
